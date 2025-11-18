c4112df32c3253d7e68edd7b95db890d86ba3356
AstroTomer <tomergueta99@gmail.com>
2025-11-18 19:00:27 +0200

    added implention to the function. Need to chnage tests and add a function that map the files from logical to phyical names, according to the TA's answer in the forum that files shouldn't have the same names as the name passed in the CLI.

--- Create/create.cpp
+++ Create/create.cpp
@@ -1,4 +1,23 @@
 #include "create.h"
-bool createFileInRleDir(const std::string& fileName){
+#include <cstdlib> 
+#include <filesystem>  
+#include <fstream>  
+namespace fs = std::filesystem;
 
+bool createFileInRleDir(const std::string& fileName){
+    const char* env = std::getenv("RLE_DIR");
+    if (!env) {
+        return false;  
+    }
+
+    fs::path dirPath(env);
+    fs::path fullPath = dirPath / fileName;
+
+    std::ofstream file(fullPath, std::ios::binary | std::ios::trunc);
+    if (!file.is_open()) {
+        return false;   // Failed to create file
+    }
+
+    file.close();
+    return true;
 }
