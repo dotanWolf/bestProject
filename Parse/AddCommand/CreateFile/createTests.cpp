29663ee3db2b17437de4cbfd734b935b89ab74d5
AstroTomer <tomergueta99@gmail.com>
2025-11-18 17:37:14 +0200

    added comments

--- Parse/AddCommand/CreateFile/createTests.cpp
+++ Parse/AddCommand/CreateFile/createTests.cpp
@@ -7,38 +7,41 @@
 
 namespace fs = std::filesystem;
 
+/* Create temp folder and set RLE_DIR to this path */
 static std::string setupRleDir()
 {
     fs::path p = fs::temp_directory_path() / "rle_tests_dir";
 
+    // Remove old test folder if exists
     if (fs::exists(p)) {
         fs::remove_all(p);
     }
     fs::create_directories(p);
 
+    // Set environment variable RLE_DIR
 #ifdef _WIN32
     _putenv_s("RLE_DIR", p.string().c_str());
 #else
     setenv("RLE_DIR", p.string().c_str(), 1);
 #endif
 
-    return p.string();           
+    return p.string();
 }
 
-// Tests 
+/* Tests */
 
 TEST(createFile, createsFileInRleDir)
 {
     std::string rleDir = setupRleDir();
 
+    // Should create the file successfully
     bool res = createFileInRleDir("newFile");
-
     ASSERT_TRUE(res);
 
+    // Check that file exists and is empty
     fs::path filePath = fs::path(rleDir) / "newFile";
     ASSERT_TRUE(fs::exists(filePath));
     ASSERT_TRUE(fs::is_regular_file(filePath));
-
     EXPECT_EQ(fs::file_size(filePath), 0u);
 }
 
@@ -46,42 +49,47 @@ TEST(createFile, canCreateMultipleFilesInSameDir)
 {
     std::string rleDir = setupRleDir();
 
+    // Create two files
     bool res1 = createFileInRleDir("file1");
     bool res2 = createFileInRleDir("file2");
 
     ASSERT_TRUE(res1);
     ASSERT_TRUE(res2);
 
+    // Both should exist
     EXPECT_TRUE(fs::exists(fs::path(rleDir) / "file1"));
     EXPECT_TRUE(fs::exists(fs::path(rleDir) / "file2"));
 }
 
 TEST(createFile, failsWhenRleDirDoesNotExist)
 {
+    // Set RLE_DIR to a path that does not exist
 #ifdef _WIN32
     _putenv_s("RLE_DIR", "C:\\this\\path\\definitely\\doesnt\\exist");
 #else
     setenv("RLE_DIR", "/this/path/definitely/doesnt/exist", 1);
 #endif
 
+    // Should fail
     bool res = createFileInRleDir("newFile");
-
     EXPECT_FALSE(res);
 }
 
 TEST(createFile, failsWhenRleDirNotSet)
 {
+    // Remove RLE_DIR from environment
 #ifdef _WIN32
     _putenv_s("RLE_DIR", "");
 #else
     unsetenv("RLE_DIR");
 #endif
 
+    // Should fail
     bool res = createFileInRleDir("newFile");
-
     EXPECT_FALSE(res);
 }
 
+/* Main for running the tests */
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
