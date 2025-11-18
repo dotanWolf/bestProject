ade9a9356171928c70cd17973865a63199788608
AstroTomer <tomergueta99@gmail.com>
2025-11-18 02:13:02 +0200

    BT-9_create_a_funtion_to_create_a_file_based_on_the_enviornment_variable

--- Parse/AddCommand/CreateFile/create.cpp
+++ Parse/AddCommand/CreateFile/create.cpp
@@ -1,4 +1,32 @@
 #include "create.h"
-bool createFileInRleDir(const std::string& fileName){
+#include <filesystem>
+#include <fstream>
+#include <cstdlib>   // std::getenv
+#include <string>
 
+namespace fs = std::filesystem;
+
+bool createFileInRleDir(const std::string& fileName)
+{
+    try {
+        const char* dirEnv = std::getenv("RLE_DIR");
+        if (dirEnv == nullptr || *dirEnv == '\0') {
+            // RLE_DIR not set
+            return false;
+        }
+
+        fs::path baseDir(dirEnv);
+
+        if (!fs::exists(baseDir) || !fs::is_directory(baseDir)) {
+            return false;
+        }
+
+        fs::path filePath = baseDir / fileName;
+
+        std::ofstream out(filePath, std::ios::binary | std::ios::trunc);
+        return out.is_open();
+    }
+    catch (const std::exception&) {
+        return false;
+    }
 }
