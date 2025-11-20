2c87820093a8f53813caf7d8213c65f674e89f05
AstroTomer <tomergueta99@gmail.com>
2025-11-20 20:33:59 +0200

    BT-35 added basic implemention for the header files

--- src/Headers/commands.h
+++ src/Headers/commands.h
@@ -1,2 +1,18 @@
 //Enum for ADD / GET / SEARCH / INVALID
 //Mapping text → command type
+#ifndef COMMANDS_H
+#define COMMANDS_H
+#include <string>
+#include <sstream>
+
+enum class CommandType {
+    ADD,
+    GET,
+    SEARCH,
+    INVALID
+};
+
+// Maps the first word of the input to a CommandType
+CommandType parseCommandType(const std::string& input);
+
+#endif 
--- src/Headers/load.h
+++ src/Headers/load.h
@@ -1,2 +1,9 @@
 //Reading a file's content from disk
 //expected for get command
+#ifndef LOAD_H
+#define LOAD_H
+
+#include <string>
+std::string getContent(std::string path);
+
+#endif
