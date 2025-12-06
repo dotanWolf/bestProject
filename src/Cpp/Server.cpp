9aadccc011d1eed490910553f61ef281eb87e6d7
AstroTomer <tomergueta99@gmail.com>
2025-12-06 15:57:20 +0200

    BT-88 added tests for server

--- src/Cpp/Server.cpp
+++ src/Cpp/Server.cpp
@@ -1,34 +1,35 @@
 #include <string> 
 #include <iostream>
-#include <sys/socket.h>
 #include <stdio.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <cstring>
+#include "Server.h"
 
 
-Server:Server(const int port, App app) {
-    this -> port = port;
-    this -> app = app;
+Server::Server(int port, const App& app)
+    : port(port), app(app)  
+{
 }
-
-int Server:CreateSocket() {
+int Server::CreateSocket() {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         perror("error creating socket");
         return -1;
     }
+    std::cout << "Socket created" << std::endl;
     return sock;
 }
 
-struct sockaddr_in Server:CreateServerAddress() {
+struct sockaddr_in Server::CreateServerAddress() {
     struct sockaddr_in sin;
     memset(&sin, 0, sizeof(sin));
     sin.sin_family = AF_INET;
     // puts a constant, which means we dont care which network card recieves the data
     // we want to get it
     sin.sin_addr.s_addr = INADDR_ANY;
-    sin.sin_port = htons(server_port);
+    sin.sin_port = htons(port);
     return sin;
 }
 
@@ -36,7 +37,6 @@ struct sockaddr_in Server:CreateServerAddress() {
 
 // }
 
-// void Server:createThreadForNewUser() {
-
-// }
-
+bool Server::createThreadForNewUser() {
+    return false;
+}
---
+++ tests/serverTests/serverTests.cpp
@@ -0,0 +1,34 @@
+#include "gtest/gtest.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <thread>
+#include "Server.h" 
+#include "ICommand.h"
+
+using namespace std;
+
+TEST(ServerProtocolTest, Validation){
+std::map<std::string, ICommand*> commands;
+ICompressor* compressor = nullptr;
+
+App app(commands, compressor);
+    Server testServer(8080, app);
+    int resultSock = testServer.CreateSocket();
+    EXPECT_GE(resultSock, 0);
+}
+
+TEST(ServerProtocolTest, ThreadCreationSuccess) {
+    std::map<std::string, ICommand*> commands;
+    ICompressor* compressor = nullptr;
+
+    App app(commands, compressor);
+    Server testServer(8080, app);
+
+    EXPECT_TRUE(testServer.createThreadForNewUser());
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
