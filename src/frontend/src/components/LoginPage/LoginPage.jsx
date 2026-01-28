3f45428b3aacf131cdf3d4e5aa41abeba0a6fec7
AstroTomer <tomergueta99@gmail.com>
2026-01-28 22:54:03 +0200

    Ex5 done

--- src/frontend/src/components/LoginPage/LoginPage.jsx
+++ src/frontend/src/components/LoginPage/LoginPage.jsx
@@ -53,7 +53,7 @@ function LoginPage() {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-              email: updatedInput[0],
+              email: updatedInput[0].toLowerCase().trim(),
               password: updatedInput[1],
             }),
           });
